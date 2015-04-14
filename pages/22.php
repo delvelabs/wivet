9b16a110d63bef1d06c971ec326164bb02bc7b69
Lukas Subel <lukas@lsubel.de>
2015-04-14 15:39:40 +0200

    Added another case where the url is split

--- pages/22.php
+++ pages/22.php
@@ -16,51 +16,43 @@
 		var socket2;
 		var button2 = document.getElementById("button2");
 		var div2 = document.getElementById("div2");
+		var socket3;
+		var button3 = document.getElementById("button3");
+		var div3 = document.getElementById("div3");
 		
-		// create a new connection when the button is clicked,
-		// then directly send a message and after receiving it close the connection
+		// override href in existing <a> with direct link
 		button1.addEventListener("click", function(event) {
 
-			// open websocket connection
 			socket1 = new WebSocket("ws://localhost:8181", "echo-protocol");
 
-			// when connection is open
 			socket1.addEventListener("open", function(event) {
 				socket1.send('../innerpages/<?php tc('22_16e11'); ?>.php');
 			});
 
-			// when receiving message
 			socket1.addEventListener("message", function(event) {
 				message1.href = event.data;
 				socket1.close();
 			});
 
-			// when error occures
 			socket1.addEventListener("error", function(event) {
 				message1.textContent = "Error: " + event;
 				socket1.close();
 			});
-
-			// when connection should be closed
+			
 			socket1.addEventListener("close", function(event) {
 			});
 		});
 	
-		// create a new connection when the button is clicked,
-		// then directly send a message and after receiving it close the connection
+		// create new <a> with direct link
 		button2.addEventListener("click", function(event) {
 
-			// open websocket connection
 			socket2 = new WebSocket("ws://localhost:8181", "echo-protocol");
 
-			// when connection is open
 			socket2.addEventListener("open", function(event) {
 				socket2.send('../innerpages/<?php tc('22_abcde'); ?>.php');
 			});
 
-			// when receiving message
 			socket2.addEventListener("message", function(event) {
-				message1.href = event.data;
 				var alink = document.createElement("a");
 				alink.href = event.data;
 		        alink.innerHTML = "click me 2";
@@ -68,24 +60,52 @@
 				socket2.close();
 			});
 
-			// when error occures
 			socket2.addEventListener("error", function(event) {
 				message2.textContent = "Error: " + event;
 				socket2.close();
 			});
 
-			// when connection should be closed
 			socket2.addEventListener("close", function(event) {
+			});
+		});
+
+		// create new <a> with created link
+		button3.addEventListener("click", function(event) {
+
+			socket3 = new WebSocket("ws://localhost:8181", "echo-protocol");
+
+			socket3.addEventListener("open", function(event) {
+				socket3.send('innerpages,<?php tc('22_8590a.php'); ?>');
+			});
+
+			socket3.addEventListener("message", function(event) {
+				var alink = document.createElement("a");
+				var substrings = event.data.split(',');
+				alink.href = "../".concat(substrings[0], "/", substrings[1]);
+		        alink.innerHTML = "click me 3";
+		        div3.appendChild(alink);
+				socket3.close();
+			});
+			
+			socket3.addEventListener("error", function(event) {
+				message3.textContent = "Error: " + event;
+				socket3.close();
+			});
+
+			socket3.addEventListener("close", function(event) {
 			});
 		});
 	});
 </script>
 
 <?php html_body();  ?>
-  <div id="div1">
-  	<input id="button1" type="button" value="Click Me" /><br />
-  	<a href=# id="message1">Link</a>
-  </div>
-  <div id="div2">
-  	<input id="button2" type="button" value="Click Me" /><br />
-  </div>
+<div id="div1">
+	<input id="button1" type="button" value="Click Me" /><br />
+	<a href=# id="message1">Link</a>
+</div>
+<div id="div2">
+	<input id="button2" type="button" value="Click Me" /><br />
+</div>
+<div id="div3">
+	<input id="button3" type="button" value="Click Me" /><br />
+</div>
